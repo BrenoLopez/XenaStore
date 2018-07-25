b7ef57ac04cb23a80a585c621401b133b79f3f9c
Unknown <greisonsantos03@gmail.com>
2018-07-25 11:38:43 -0300

    Revert "Merge remote-tracking branch 'origin/BrenoLopes' into greison"
    This reverts commit 97499259506ae9e8d4ee4fd49696e56a0ee11316.

--- motor/classes/produto.php
+++ motor/classes/produto.php
@@ -238,11 +238,6 @@
 		public function buscaProd(){
 
 			$sql = "select * from produto where category = '$this->category' and like name_product = '%$this->itemBuscado%'";
-            $DB = new DB();
-            $DB->open();
-            $result = $DB->query($sql);
-            $DB->close();
-            return $result;
 
 		}
 		
